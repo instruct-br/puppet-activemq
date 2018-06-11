4cba23249159078eac22e46727b5ec71f91ec2c7
Guto Carvalho <gutocarvalho@instruct.com.br>
2018-06-11 20:00:04 -0300

    Added TLS support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,19 @@
 # Class activemq
 
 class activemq (
-  $version            = '5.15.2',
-  $checksum           = 'ed2da381eeea5d0d43601f4e97d91bb8',
-  $checksum_type      = 'md5',
-  $memory             = '-Xms512m -Xmx512m',
-  $home               = '/opt/activemq',
-  $user               = 'activemq',
-  $group              = 'activemq',
-  $mco_config         = true,
-  $mco_user           = 'mcollective',
-  $mco_pass           = 'p4ssw0rd',
-  $system_config_path = '/etc/sysconfig',
+  $version             = '5.15.2',
+  $checksum            = 'ed2da381eeea5d0d43601f4e97d91bb8',
+  $checksum_type       = 'md5',
+  $memory              = '-Xms512m -Xmx512m',
+  $home                = '/opt/activemq',
+  $user                = 'activemq',
+  $group               = 'activemq',
+  $mco_config          = true,
+  $mco_user            = 'mcollective',
+  $mco_pass            = 'p4ssw0rd',
+  $system_config_path  = '/etc/sysconfig',
+  $truststore_password = 'p4ssw0rd',
+  $keystore_password   = 'p4ssw0rd',
   ) {
 
   include activemq::install
--- manifests/mco.pp
+++ manifests/mco.pp
@@ -2,13 +2,38 @@
 
 class activemq::mco {
 
+  file { '/etc/activemq/ssl':
+    ensure => directory,
+    mode   => '0644',
+    owner  => $activemq::user,
+    group  => $activemq::group,
+  }
+
+  java_ks { 'puppet_ca_truststore':
+    ensure       => latest,
+    certificate  => '/etc/puppetlabs/puppet/ssl/certs/ca.pem',
+    target       => '/etc/activemq/ssl/truststore.ts',
+    password     => $activemq::truststore_password,
+    trustcacerts => true,
+  }
+
+  java_ks { 'puppet_agent_keystore':
+    ensure      => latest,
+    certificate => "/etc/puppetlabs/puppet/ssl/certs/${::trusted['certname']}.pem",
+    private_key => "/etc/puppetlabs/puppet/ssl/private_keys/${::trusted['certname']}.pem",
+    target      => '/etc/activemq/ssl/keystore.ks',
+    password    => $activemq::keystore_password,
+  }
+
   file { '/etc/activemq/activemq.xml':
     ensure  => file,
     owner   => $activemq::user,
     group   => $activemq::group,
     content => epp('activemq/activemq.xml.epp',{
-      mco_user => $activemq::mco_user,
-      mco_pass => $activemq::mco_pass,
+      mco_user            => $activemq::mco_user,
+      mco_pass            => $activemq::mco_pass,
+      keystore_password   => $activemq::keystore_password,
+      truststore_password => $activemq::truststore_password,
     }),
     notify  => Service['activemq'],
   }
